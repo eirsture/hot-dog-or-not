db65e542b64d7b76ad466abaa9f69ec314d68ff0
eirsture <eirik.sture@gmail.com>
2020-11-23 23:34:48 +0100

    improve styling and finalizations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Card from './Components/Card';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHotdog, faDog } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [renderCard, setRenderCard] = useState(true);
+
+  const clearStorage = () => {
+    sessionStorage.removeItem('dogs');
+  }
+
+  const renderContent = () => {
+    if (renderCard) {
+      return <Card className='Card'/>
+    } else {
+      const dogList = JSON.parse(sessionStorage.getItem('dogs')) || [];
+
+      return (<div onClick={clearStorage} className="imageList">{dogList.map((dog, i) => {
+        return <img alt="dog" key={i} src={dog} />
+      })}</div>);
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Header'>
+      <FontAwesomeIcon className="Button cross" icon={faHotdog} size="lg" onClick={() => setRenderCard(false)} />
+      <FontAwesomeIcon className="Button heart" icon={faDog} size="lg" onClick={() => setRenderCard(true)}/>
+      </div>
+      {renderContent()}
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import './Card.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes, faHeart, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import logo from '../logo.svg';
+
+function Card() {
+  const [dogImage, setDogImage] = useState();
+  const [initialMouseX, setInitialMouseX] = useState();
+
+  const refTest = useRef(null);
+  const fetchRandomDog = async () => {
+    const result = await axios('https://dog.ceo/api/breeds/image/random');
+    if (result.data.status === 'success') {
+      setDogImage(result.data.message);
+      return result.data.message;
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomDog();
+    document.onanimationend = () => {
+      refTest.current.classList.remove('right-trigger');
+      refTest.current.classList.remove('left-trigger');
+    };
+  }, []);
+
+  const handleTouchStart = (event) => {
+    if (event.nativeEvent instanceof TouchEvent) {
+      setInitialMouseX(event.touches[0].clientX);
+    } else if (event.nativeEvent instanceof MouseEvent) {
+      const mouseX = event.clientX;
+      setInitialMouseX(event.clientX);
+
+      document.addEventListener(
+        'mouseup',
+        (e) => {
+          handleTouchEnd(mouseX, e.clientX);
+        },
+        { once: true }
+      );
+    }
+  };
+
+  const handleTouchEnd = (initial, final) => {
+    if (initial - final >= 60) {
+      handleLeft();
+    } else if (initial - final <= -60) {
+      handleRight();
+    }
+    setInitialMouseX(null);
+  };
+
+  const handleLeft = async () => {
+    refTest.current.classList.add('left-trigger');
+    await fetchRandomDog();
+  };
+
+  const handleRight = async () => {
+    refTest.current.classList.add('right-trigger');
+    const dogList = JSON.parse(sessionStorage.getItem('dogs')) || [];
+    dogList.push(dogImage);
+    sessionStorage.setItem('dogs', JSON.stringify(dogList));
+    await fetchRandomDog();
+  };
+
+  return (
+    <div
+      className='Card'
+      ref={refTest}
+      onMouseDown={handleTouchStart}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={(event) =>
+        handleTouchEnd(initialMouseX, event.changedTouches[0].clientX)
+      }
+    >
+      <div className='img-wrapper'>
+      {!dogImage ? (
+        <FontAwesomeIcon className='Loading' icon={faSpinner} size="2x" />
+      ) : (
+        <img src={dogImage} alt='Dog' />
+        
+      )}
+      </div>
+      <div className='Buttons'>
+        <FontAwesomeIcon className="Button cross" onClick={handleLeft} icon={faTimes} size="2x" />
+        <FontAwesomeIcon className="Button heart" onClick={handleRight} icon={faHeart} size="2x" />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
