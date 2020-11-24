cf918e8a2f2e029e3ac3b983695b40231a8a44dd
eirsture <eirik.sture@gmail.com>
2020-11-24 13:57:54 +0100

    fix rendering of liked dogs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Components/Card';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHotdog, faDog } from '@fortawesome/free-solid-svg-icons'
+import { faHotdog, faDog, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [renderCard, setRenderCard] = useState(true);
+  const [likedDogs, setLikedDogs] = useState([])
+
+  useEffect(() => {
+    const dogList = JSON.parse(sessionStorage.getItem('dogs')) || [];
+    setLikedDogs(dogList);
+  }, [renderCard]);
 
   const clearStorage = () => {
     sessionStorage.removeItem('dogs');
   }
 
-  const renderContent = () => {
+  const renderContent = (dogList) => {
     if (renderCard) {
       return <Card className='Card'/>
     } else {
-      const dogList = JSON.parse(sessionStorage.getItem('dogs')) || [];
-
+      if (dogList.length === 0) {
+        return (<div className="infoText">
+                  <h3>Could not find any dogs you have liked</h3>
+                  <div><FontAwesomeIcon className="red" icon={faHeartBroken} size="5x"/></div></div>)
+      } else {
       return (<div onClick={clearStorage} className="imageList">{dogList.map((dog, i) => {
         return <img alt="dog" key={i} src={dog} />
       })}</div>);
-    }
+    }}
   }
 
-  
+  const selected = (bool) => bool ? "highlight" : "faded"
 
   return (
     <div className='App'>
       <div className='Header'>
-      <FontAwesomeIcon className="Button cross" icon={faHotdog} size="lg" onClick={() => setRenderCard(false)} />
-      <FontAwesomeIcon className="Button heart" icon={faDog} size="lg" onClick={() => setRenderCard(true)}/>
+      <FontAwesomeIcon className={`Button cyan ${selected(!renderCard)}`} icon={faHotdog} size="2x" onClick={() => setRenderCard(false)} />
+      <FontAwesomeIcon className={`Button red ${selected(renderCard)}`} icon={faDog} size="2x" onClick={() => setRenderCard(true)}/>
       </div>
-      {renderContent()}
+      {renderContent(likedDogs)}
     </div>
   );
 }
